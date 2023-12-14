dff55f8c67311a2876c4d393365dffbfeb75ebb9
Darla Young <89855627+Darla-Young@users.noreply.github.com>
2023-12-14 10:00:09 -0600

    middleware drafted, untested

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,49 +1,39 @@
-/*
-  If the user does not have a session saved in the server
+const Users = require('../users/users-model')
 
-  status 401
-  {
-    "message": "You shall not pass!"
-  }
-*/
-function restricted() {
-
+  // If the user does not have a session saved in the server:
+  //   status 401 message: "You shall not pass!"
+function restricted(req, res, next) {
+  if (req.session && req.session.user_id) next()
+  else res.status(401).json({message: "You shall not pass!"})
 }
 
-/*
-  If the username in req.body already exists in the database
-
-  status 422
-  {
-    "message": "Username taken"
-  }
-*/
-function checkUsernameFree() {
-
+  // If the username in req.body already exists in the database:
+  //   status 422 message: "Username taken"
+async function checkUsernameFree(req, res, next) {
+  const existing = await Users.findBy('username', '=', req.body.username)
+  if (existing[0]) res.status(422).json({message: "Username taken"})
+  else next()
 }
 
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
-*/
-function checkUsernameExists() {
-
+  // If the username in req.body does NOT exist in the database:
+  //   status 401 message: "Invalid credentials"
+async function checkUsernameExists(req, res, next) {
+  const existing = await Users.findBy('username', '=', req.body.username)
+  if (!existing[0]) res.status(401).json({message: "Invalid credentials"})
+  else next()
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
-*/
-function checkPasswordLength() {
-
+  // If password is missing from req.body, or if it's 3 chars or shorter:
+  //   status 422 message: "Password must be longer than 3 chars"
+function checkPasswordLength(req, res, next) {
+  const { password } = req.body
+  if (!password || password.length < 4) res.status(422).json({message: "Password must be longer than 3 chars"})
+  else next()
 }
 
-// Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength,
+}
--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,29 +1,21 @@
 const db = require('../../data/db-config')
 
-/**
-  resolves to an ARRAY with all users, each user having { user_id, username }
- */
+  // resolves to an ARRAY with all users, each user having { user_id, username }
 function find() {
   return db('users')
 }
 
-/**
-  resolves to an ARRAY with all users that match the filter condition
- */
+  // resolves to an ARRAY with all users that match the filter condition
 function findBy(filter) {
   return db('users').where(filter)
 }
 
-/**
-  resolves to the user { user_id, username } with the given user_id
- */
+  // resolves to the user { user_id, username } with the given user_id
 function findById(user_id) {
   return db('users').where('user_id', '=', user_id).first()
 }
 
-/**
-  resolves to the newly inserted user { user_id, username }
- */
+  // resolves to the newly inserted user { user_id, username }
 async function add(user) {
   const id = await db('users').insert(user).first()
   const newUser = findById(id)
@@ -31,7 +23,6 @@ async function add(user) {
   return newUser
 }
 
-// Don't forget to add these to the `exports` object so they can be required in other modules
 module.exports = {
   find,
   findBy,
